ea0d57c0fd047f087ed384d73ab174c72ccde852
takunology <takunology.net@gmail.com>
2022-07-01 19:10:45 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
 }
 
 module.exports = {
-<<<<<<< HEAD
-  basePath: process.env.GITHUB_ACTIONS ? "/" : "",
-=======
   basePath: process.env.GITHUB_ACTIONS ? "/takunology.github.io/fw.mcwithcode.com" : "",
->>>>>>> parent of c2c4147 (Update next.config.js)
   trailingSlash: true,
 };
