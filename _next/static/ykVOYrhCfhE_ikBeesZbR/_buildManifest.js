320c5c0de9eb972215461616e06e0b9cc3f8134e
takunology <takunology@users.noreply.github.com>
2022-08-21 16:45:41 +0000

    deploy: b1df6f1e71613ad9883e1f10e9be759683f07380

---
+++ _next/static/ykVOYrhCfhE_ikBeesZbR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-dc70fd2ff5e601e0.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/layout":["static/chunks/pages/layout-422a4e12ff048d23.js"],sortedPages:["/","/_app","/_error","/layout"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
